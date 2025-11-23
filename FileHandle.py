191013e92742e306223af447f299f87a3925d960
Arthi26AS <arthiselvaraj.as@gmail.com>
2025-11-23 16:51:09 +0530

    feat: Added new code file handle

---
+++ FileHandle.py
@@ -0,0 +1,12 @@
+import os
+
+filepath = "D:\\python\\new.txt"
+
+if os.path.exists(filepath):
+    with open(filepath, "a") as f:
+        f.write("Mr.H is good")
+else:
+    # open in write mode (creates new file)
+    # write data
+    with open(filepath, "w") as f:
+        f.write("Mr.H is bad")
